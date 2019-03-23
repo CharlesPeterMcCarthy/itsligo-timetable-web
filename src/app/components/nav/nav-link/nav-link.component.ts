9668d2d40bae694ab8bcb80031a5c4a27233e191
CharlesPeterMcCarthy <charles@studybundles.com>
2019-03-23 10:21:42 +0000

    Highlight Active Nav Link

--- src/app/components/nav/nav-link/nav-link.component.ts
+++ src/app/components/nav/nav-link/nav-link.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { NavLink } from '../../../interfaces/nav-link';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'nav-link',
@@ -11,8 +12,10 @@ export class NavLinkComponent implements OnInit {
 
   @Input() public navLink: NavLink;
 
-  constructor() { }
+  constructor(private _router: Router) { }
 
   ngOnInit() { }
+
+  public IsActive = (): boolean => this._router.url === this.navLink.url;
 
 }
